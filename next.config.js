927c4bf508e1e058c4ad7e15cadeab93e7e829aa
Wollemanlech077 <aordazm180@gmail.com>
2025-10-05 00:49:29 -0600

    Fix build error - Remove experimental optimizeCss flag

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   // appDir is now stable in Next.js 15, no need for experimental flag
   outputFileTracingRoot: __dirname,
-  experimental: {
-    // Optimizaciones para mejor rendimiento
-    optimizeCss: true,
-  },
 }
 
 module.exports = nextConfig
